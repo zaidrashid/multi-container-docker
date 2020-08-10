1c630994dfd73fd875ba0358ae5b42427f5fda2d
Zaid Rashid <zed182@gmail.com>
2020-08-10 07:41:47 +0200

    Init Commit

---
+++ worker/index.js
@@ -0,0 +1,20 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
+});
+const subscription = redisClient.duplicate();
+
+function fib(index) {
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index -2);
+}
+
+subscription.on('message', (channnel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message)));
+});
+
+subscription.subscribe('insert');
